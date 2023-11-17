d9275737031979546f4ae2707119d242291e142c
EhzandHerry <ehzand.herry1926@gmail.com>
2023-11-17 13:56:13 +0700

    mengubah FormUIState

--- app/src/main/java/com/example/ucp2/FormViewModel.kt
+++ app/src/main/java/com/example/ucp2/FormViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.ucp2
 
-class FormViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class FormViewModel : ViewModel(){
+    private val _stateUI = MutableStateFlow(FormViewModel())
+    val stateUI: StateFlow<FormViewModel> = _stateUI.asStateFlow()
+
+    fun setForm(listData: MutableList<String>){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                nama = listData[0],
+                nim = listData[1],
+                konsentrasi = listData[2],
+                judul = listData[3]
+            )
+        }
+    }
 }
