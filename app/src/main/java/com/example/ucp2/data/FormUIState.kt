2c0cfff15ca97eb40c150c04315d59bbab935bc7
EhzandHerry <ehzand.herry1926@gmail.com>
2023-11-17 13:45:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ucp2/data/FormUIState.kt
@@ -0,0 +1,8 @@
+package com.example.ucp2.data
+
+class FormUIState {
+    val nama: String = ""
+    val nim: String = ""
+    val konsentrasi: String = ""
+    val judul: String = ""
+}
---
+++ app/src/main/java/com/example/ucp2/data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2.data
+
+object SumberData {
+    val dosen1 = listOf(
+        "Mulyadi",
+        "Prasetyo",
+        "Dimas"
+    )
+    val dosen2 = listOf(
+        "Siti",
+        "Fatimah",
+        "Susi"
+    )
+}
